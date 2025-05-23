b7373509b2809fbc6890ef8140acbfa289ca4aa2
Ekaterina <ek.beidel@gmail.com>
2025-05-23 13:53:24 +0200

    add command dispatcher

--- src/Shared/BuildingBlocks/Core/GlobalUsing.cs
+++ src/Shared/BuildingBlocks/Core/GlobalUsing.cs
@@ -1,1 +1,2 @@
 global using Core.Messages;
+global using Core.Commands;
---
+++ src/Shared/BuildingBlocks/Core/MediatR/ICommandDispatcher.cs
@@ -0,0 +1,8 @@
+namespace Core.MediatR;
+
+public interface ICommandDispatcher
+{
+    void RegisterHandler<T>(Func<T, Task> handler)
+        where T : BaseCommand;
+    Task SendCommandAsync(BaseCommand command);
+}
