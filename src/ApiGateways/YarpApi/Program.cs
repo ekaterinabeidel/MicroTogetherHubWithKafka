204ad402088d071aaa2348f894ba5a2f9efdff59
Ekaterina <ek.beidel@gmail.com>
2025-05-24 23:21:59 +0200

    add configuration for request rate limiting

--- src/ApiGateways/YarpApi/Program.cs
+++ src/ApiGateways/YarpApi/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("YarpProxy"));
+builder.Services.AddRateLimiter(option =>
+{
+    option.AddFixedWindowLimiter("fixed", config =>
+    {
+        config.PermitLimit = 3;
+        config.Window = TimeSpan.FromSeconds(30);
+    });
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+app.MapReverseProxy().RequireRateLimiting("fixed");
 
 app.Run();
